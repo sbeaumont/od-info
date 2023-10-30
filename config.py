ee0fabb6541decf6b4e98d7dd6206d1b645b4863
Serge Beaumont <sbeaumont@xebia.com>
2023-10-30 15:44:58 +0100

    Improved comments

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import sys
 import os
 
+# Template of the secrets.txt file that gets saved when it can't be found.
+
 SECRETS_TEMPLATE = """Remove this line and all the <...> sections to make it work.
 username = <your OD username>
 password = <your OD password>
@@ -9,8 +11,12 @@ current_player_id = <Five number id of your player this round>
 LOCAL_TIME_SHIFT = 0 <(Negative) number. If you see the timing of the app being off, this allows you to correct it.>
 """
 
+# Utility functions
+# The getattr() in these functions checks determines whether we're running as a pyinstaller binary.
+
 
 def resource_path(rel_path: str):
+    """Changes given relative path in case we're a pyinstaller binary version"""
     if getattr(sys, 'frozen', False):
         return os.path.join(sys._MEIPASS, rel_path)
     else:
@@ -18,6 +24,7 @@ def resource_path(rel_path: str):
 
 
 def executable_path(rel_path: str):
+    """Gives the correct work directory depending on running from the project of as a pyinstaller binary version"""
     if getattr(sys, 'frozen', False):
         return os.path.join(os.path.dirname(sys.executable), rel_path)
     else:
@@ -25,6 +32,7 @@ def executable_path(rel_path: str):
 
 
 def load_secrets():
+    """Load secrets.txt configuration file."""
     secrets_filename = executable_path('secret.txt')
     if not os.path.exists(secrets_filename):
         with open(secrets_filename, 'w') as f:
@@ -40,20 +48,22 @@ def load_secrets():
         return secrets_dict
 
 
+# Make configuration in secrets.txt available to the rest of the codebase
+
 SECRETS = load_secrets()
 username = SECRETS['username']
 password = SECRETS['password']
 current_player_id = int(SECRETS['current_player_id'])
 LOCAL_TIME_SHIFT = int(SECRETS['LOCAL_TIME_SHIFT'])
-discord_webhook = SECRETS['discord_webhook']
+discord_webhook = SECRETS.get('discord_webhook', None)
+
+# Use this to make features toggleable (typically screens in development)
 
 feature_toggles = []
 if 'feature_toggles' in SECRETS:
     feature_toggles = [toggle.strip() for toggle in SECRETS['feature_toggles'].split(',')]
 
-OUT_DIR = './out'
-REF_DATA_DIR = './ref-data'
-OPS_DATA_DIR = 'opsdata'
+# Location and name of database file. schema.sql is what initializes a new database.
 
 if getattr(sys, 'frozen', False):
     DATABASE = executable_path('odinfo-database.sqlite')
@@ -62,20 +72,34 @@ else:
 
 DB_SCHEMA_FILE = resource_path('./opsdata/schema.sql')
 
-OD_BASE = 'https://www.opendominion.net'
+# The correct handling of date/timestamps in combination with the sqlite database is dependent
+# on this being set correctly. Only change this when you change underlying DB tech.
 
+DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# Knowledge of internal directory structure
+
+OUT_DIR = './out'
+REF_DATA_DIR = './ref-data'
+OPS_DATA_DIR = 'opsdata'
+
+# Knowledge of the URL structure of the OD website
+
+OD_BASE = 'https://www.opendominion.net'
 LOGIN_URL = f'{OD_BASE}/auth/login'
 SEARCH_PAGE = f'{OD_BASE}/dominion/search'
 OP_CENTER_URL = f'{OD_BASE}/dominion/op-center'
 TOWN_CRIER_URL = f'{OD_BASE}/dominion/town-crier'
 STATUS_URL = f'{OD_BASE}/dominion/status'
 SELECT_URL = f'{OD_BASE}/dominion/' + '{}/select'
-
 MY_OP_CENTER_URL = f'{OD_BASE}/dominion/advisors/op-center'
+
+# Probably deprecated, older versions used files extensively.
 
 DOM_INDEX = f'{OUT_DIR}/dom_index.json'
 NETWORTH_FILE = f'{OUT_DIR}/nw.json'
-DATE_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+# Global settings that can't be found anywhere else
 
 PLAT_PER_ALCHEMY_PER_TICK = 45
 PLAT_PER_PEASANT_PER_TICK = 2.7

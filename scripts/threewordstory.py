80ba4da88af5c2ccb1362ccd69cd7c42b677d6e5
Serge Beaumont <sbeaumont@xebia.com>
2024-01-24 16:33:40 +0100

    Change Safe OP/DP headers based on regular or VS listing.

--- scripts/threewordstory.py
+++ scripts/threewordstory.py
@@ -3,19 +3,23 @@ import time
 from config import current_player_id
 from opsdata.scrapetools import login, get_soup_page
 
-FORUM_URL = 'https://www.opendominion.net/dominion/forum/327?page={}'
+FORUM_URL = 'https://www.opendominion.net/dominion/forum/330?page={}'
 
 
 def go():
     comments = list()
     session = login(current_player_id)
     for page_nr in range(1, 99):
-        soup = get_soup_page(session, FORUM_URL.format(page_nr))
-        if not soup:
-            break
-        for comment in soup.find_all('div', {'class': 'comment-text'}):
+        url = FORUM_URL.format(page_nr)
+        print(f"Loading page {url}")
+        soup = get_soup_page(session, url)
+        comments = soup.find_all('div', {'class': 'comment-text'})
+        print("COMMENTS", comments)
+        for comment in comments:
+            print(comment)
+            print(comment.p.text)
             comments.append(comment.p.text)
-    with open('threewordstory-R37.txt', 'w') as f:
+    with open('threewordstory-R38.txt', 'w') as f:
         f.write(' '.join(comments))
 
 

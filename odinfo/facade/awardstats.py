e8d122e319c1640d2a6605ab5d168fbe8ce7081d
Serge Beaumont <sbeaumont@xebia.com>
2025-11-30 22:35:10 +0100

    Better Award Stats page.

--- odinfo/facade/awardstats.py
+++ odinfo/facade/awardstats.py
@@ -8,106 +8,105 @@ class AwardStats(object):
     @property
     def bouncy_castle_award(self):
         query = text("""
-            select 
-                target as code, 
-                target_name as name, 
+            select
+                tc.target as code,
+                tc.target_name as name,
+                d.race as race,
+                d.realm as realm,
                 count(1) as amount
-            from TownCrier
-            where event_type = 'bounce'
-            group by target
+            from TownCrier tc
+            left join Dominions d on tc.target = d.code
+            where tc.event_type = 'bounce'
+            group by tc.target
             order by amount desc""")
         return self.db.session.execute(query)
 
     @property
     def bouncy_loser_award(self):
-        query = text("""select
-                            origin as code,
-                            origin_name as name, 
-                            count(1) as amount
-                        from
-                            TownCrier
-                        where
-                            event_type = 'bounce'
-                        group by
-                            origin
-                        order by
-                            amount desc""")
+        query = text("""
+            select
+                tc.origin as code,
+                tc.origin_name as name,
+                d.race as race,
+                d.realm as realm,
+                count(1) as amount
+            from TownCrier tc
+            left join Dominions d on tc.origin = d.code
+            where tc.event_type = 'bounce'
+            group by tc.origin
+            order by amount desc""")
         return self.db.session.execute(query)
 
     @property
     def war_declarations(self):
-        query = text("""select
-                            origin,
-                            origin_name,
-                            count(1) as declarations
-                        from TownCrier
-                        where
-                            event_type like 'war_declare'
-                        group by
-                            origin
-                        order by
-                            declarations desc;""")
+        query = text("""
+            select
+                tc.origin as realm,
+                tc.origin_name as name,
+                count(1) as declarations
+            from TownCrier tc
+            where tc.event_type like 'war_declare'
+            group by tc.origin
+            order by declarations desc""")
         return self.db.session.execute(query)
 
     @property
     def declared_on(self):
-        query = text("""select
-                            target,
-                            target_name,
-                            count(1) as declared_on
-                        from TownCrier
-                        where
-                            event_type like 'war_declare'
-                        group by
-                            target
-                        order by
-                            declared_on desc;""")
+        query = text("""
+            select
+                tc.target as realm,
+                tc.target_name as name,
+                count(1) as declared_on
+            from TownCrier tc
+            where tc.event_type like 'war_declare'
+            group by tc.target
+            order by declared_on desc""")
         return self.db.session.execute(query)
 
     @property
     def hits_taken(self):
-        query = text("""select
-                            target as code,
-                            target_name as name,
-                            sum(amount) as total_land,
-                            count(1) as total_hits
-                        from
-                            TownCrier
-                        group by
-                            target
-                        having
-                            total_land > 0
-                        order by
-                            total_land desc;""")
+        query = text("""
+            select
+                tc.target as code,
+                tc.target_name as name,
+                d.race as race,
+                d.realm as realm,
+                sum(tc.amount) as total_land,
+                count(1) as total_hits
+            from TownCrier tc
+            left join Dominions d on tc.target = d.code
+            group by tc.target
+            having total_land > 0
+            order by total_land desc""")
         return self.db.session.execute(query)
 
     @property
     def hits_done(self):
-        query = text("""select
-                            origin as code,
-                            origin_name as name,
-                            sum(amount) as total_land,
-                            count(1) as total_hits
-                        from
-                            TownCrier
-                        group by
-                            origin
-                        having
-                            total_land > 0
-                        order by
-                            total_land desc;""")
+        query = text("""
+            select
+                tc.origin as code,
+                tc.origin_name as name,
+                d.race as race,
+                d.realm as realm,
+                sum(tc.amount) as total_land,
+                count(1) as total_hits
+            from TownCrier tc
+            left join Dominions d on tc.origin = d.code
+            group by tc.origin
+            having total_land > 0
+            order by total_land desc""")
         return self.db.session.execute(query)
 
     @property
     def abandons(self):
-        query = text("""select
-                            origin as code,
-                            origin_name as name,
-                            target as realm
-                        from
-                            TownCrier
-                        where
-                            event_type like 'abandon'
-                        group by
-                            realm;""")
+        query = text("""
+            select
+                tc.origin as code,
+                tc.origin_name as name,
+                d.race as race,
+                tc.target as realm
+            from TownCrier tc
+            left join Dominions d on tc.origin = d.code
+            where tc.event_type like 'abandon'
+            group by tc.target""")
         return self.db.session.execute(query)
